f7f4e8acd88226fc48ee874025cdd0266e7e65c8
StavropoulosPanagiotis <156346473+StavropoulosPanagiotis@users.noreply.github.com>
2024-11-18 20:53:19 +0200

    Initial commit

---
+++ Source-1C.cpp
@@ -0,0 +1,31 @@
+﻿//********************************
+//Αυτό το αρχείο θα το χρησιμοποιήσετε
+// για να υλοποιήσετε την άσκηση 1B της OpenGL
+//
+//ΑΜ:                         Όνομα:
+//ΑΜ:                         Όνομα:
+
+//*********************************
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+using namespace std;
